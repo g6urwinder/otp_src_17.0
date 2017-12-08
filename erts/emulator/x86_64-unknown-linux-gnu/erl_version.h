49e92505a1a3864cacf63abd08a4a00f915621d0
gurwinder <thisisgurwinder@gmail.com>
2017-12-08 10:00:19 +0000

    first commit

---
+++ erts/emulator/x86_64-unknown-linux-gnu/erl_version.h
@@ -0,0 +1,6 @@
+/* This file was created by 'make_version' -- don't modify. */
+#define ERLANG_OTP_RELEASE "17"
+#define ERLANG_OTP_VERSION "17.0"
+#define ERLANG_VERSION "6.0"
+#define ERLANG_COMPILE_DATE "Fri Dec  8 09:39:35 2017"
+#define ERLANG_ARCHITECTURE "x86_64-unknown-linux-gnu"
